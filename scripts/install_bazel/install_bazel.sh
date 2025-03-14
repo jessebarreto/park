fab05c2f813cd731dd2b6662f3601d70a207a66c
jessebarreto <jesseh.barreto@gmail.com>
2025-03-13 21:12:52 -0300

    Update bazel install cmds (#8)
    * Update gitignore file for bazel & py files
    
    * Update scripts/install_bazel for Ubuntu24
    
    Also:
    Add missing version cmd when finished

--- scripts/install_bazel/install_bazel.sh
+++ scripts/install_bazel/install_bazel.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 
-sudo apt install apt-transport-https curl gnupg -y
-curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor >bazel-archive-keyring.gpg
-sudo mv bazel-archive-keyring.gpg /usr/share/keyrings
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+#
+# Script for installation in Ubuntu 24.04
+#
 
-sudo apt update && sudo apt install bazel
+LATEST_BAZELISK_DEB_LINK="https://github.com/bazelbuild/bazelisk/releases/download/v1.25.0/bazelisk-amd64.deb"
+BAZELISK_README_LINK="https://github.com/bazelbuild/bazelisk"
+
+SUPPORTED_DISTRO="24.04"
+
+containSubString() {
+    if [[ $2 == *$1* ]]; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+distro=$(lsb_release -r | grep $SUPPORTED_DISTRO)
+
+if containSubString "$SUPPORTED_DISTRO" "$distro"; then
+    echo "Not supported distro: see \"$BAZELISK_README_LINK\""
+    exit 1;
+fi
+
+mkdir -p bazel-bin/deb
+wget $LATEST_BAZELISK_DEB_LINK
+mv bazelisk-amd64.deb bazel-bin/deb
+sudo dpkg -i ./bazel-bin/deb/bazelisk-amd64.deb
+bazelisk
+bazel version
